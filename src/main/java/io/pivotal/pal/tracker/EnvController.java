907bda15e8bd6dd060f349d9cda21964714d2b85
Trenell Galman <trenellgalman@gmail.com>
2021-01-18 21:11:17 +0000

    Added the env controller. Added environment variables to the application.

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,2 +1,23 @@
-package io.pivotal.pal.tracker;public class EnvController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final Map<String, String> environment;
+
+    public EnvController(@Value("${PORT:0}") String port, @Value("${MEMORY_LIMIT:0}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:0}") String instanceIndex, @Value("${CF_INSTANCE_ADDR:Not Set}") String instanceAddress) {
+        environment = Map.of("PORT", port, "MEMORY_LIMIT", memoryLimit, "CF_INSTANCE_INDEX", instanceIndex, "CF_INSTANCE_ADDR", instanceAddress);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return environment;
+    }
 }
